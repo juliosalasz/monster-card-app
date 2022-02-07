15dca3078d556154d974189df045eaa79e5b31da
kloj00 <jscomic@gmail.com>
2022-02-07 12:16:32 -0500

    Title style changes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1 className="Title">Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search Monsters"
           handleChange={this.handleChange}
